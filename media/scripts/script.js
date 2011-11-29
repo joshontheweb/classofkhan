77c18d541086294cce0aafcfa4302ee71ded254a
Josh Nielsen <josh@joshontheweb.com>
2011-11-30 03:32:51 +1300

    over night code session done

--- media/scripts/script.js
+++ media/scripts/script.js
@@ -2,36 +2,320 @@
     Backbone.emulateHTTP = true;
     Backbone.emulateJSON = true;
 
+    Backbone.Model.idAttribute = '_id';
+
     // underscore template languate settings
     _.templateSettings = {
       interpolate : /\{\{(.+?)\}\}/g, // {{ var }}
       evaluate: /\{\%(.+?)\%\}/g // {% expression %}
     };
+    
+    // don't start app until authenticated
+    if (jsonVars.everyauth.loggedIn) {
 
-    var socket = io.connect('http://localhost:3001');
-    socket.on('news', function (data) {
-        console.log(data);
-        socket.emit('my other event', { my: 'data' });
-    });
 
-    $('.create-project').click(function(e) {
-        e.preventDefault();
-        var $form = $('.global.stash .project-form').clone(true).hide();
-        $('body').append($form);
-        $form.fadeIn();
-    });
 
-    $('.project-form .cancel').click(function(e) {
-        e.preventDefault();
-        $(e.target).closest('.project-form').fadeOut('fast', function() {
-            $(this).remove();
+
+        // Models
+        var App = Backbone.Model.extend({
+            defaults: {
+                'user': null
+            },
+            
+            start: function(data) {
+                this.users = new Users();
+                
+                this.classRoom = new ClassRoom(data);
+                this.classRoomView = new ClassRoomView({model: this.classRoom, el: $('.classroom')});
+                this.classRoomView.render();
+
+                this.chatLog = new ChatLog();
+                this.chatLogView = new ChatLogView({model: this.chatLog})
+                this.chatLogView.render();
+            }
         });
-    });
+        var AppView = Backbone.View.extend({
+            events: {
+                'keypress': 'keypress'
+            },
 
-    $('.project-form .submit').click(function() {
-        $form = $(this).closest('.project-form');
-        var title = $form.find('.project-title').val();
-        var mediaId = $form.find('.project-media-url').val().match(/[0-9a-zA-Z_\-]{11}/);
-        socket.emit('project:create', {title: title, media: mediaId, userId: jsonVars.user._id});
-    });
+            keypress: function(e) {
+                var user = app.get('user');
+                if (user && !user.get('chatting') && (e.keyCode != 13)) {
+                    user.set({'chatting': e});
+                }
+            },
+        });
+
+        var User = Backbone.Model.extend({});
+        var UserView = Backbone.View.extend({
+            initialize: function() {
+                _.bindAll(this, 'remove', 'handleChat');
+                this.model.bind('remove', this.remove);
+                this.model.bind('change:chatting', this.handleChat);
+            },
+            
+            template: _.template($('.user-template').html()),
+
+            className: 'user',
+
+            handleChat: function(model, chatting) {
+                var view = this;
+                if (!chatting) { return; }
+            
+                var $user = $(this.el);
+                var $chatBox = $('<textarea rows="10" cols="20" class="chat-box"></textarea>'); 
+                $user.append($chatBox);
+                $chatBox.focus();
+                $chatBox.bind('keypress', function(e) {
+                    if (e.keyCode == 13) {
+                        e.preventDefault();
+                        var message = new Message({content: $chatBox.val()})
+                        app.chatLog.messages.add(message);
+                        $chatBox.fadeOut(2000, function() {
+                            $chatBox.remove();
+                        });
+                        view.model.set({'chatting': false});
+                    }    
+                })
+            },
+            
+            render: function() {
+                $(this.el).html(this.template(this.model.toJSON()));
+                return this;
+            }
+        })
+
+        var Users = Backbone.Collection.extend({
+            model: User
+        });
+    
+        var ClassRoom = Backbone.Model.extend({
+            initialize: function() {
+                this.desks = new Desks();
+                
+            }
+        });
+        
+        var ClassRoomView = Backbone.View.extend({
+            initialize: function() {
+                _.bindAll(this, 'insertDesk', 'assignUserToDesk', 'removeUserFromDesk');
+                this.model.desks.bind('add', this.insertDesk);
+                app.users.bind('add', this.assignUserToDesk);
+                app.users.bind('remove', this.removeUserFromDesk);
+            },
+            
+            template: _.template($('.classroom-template').html()),
+
+            insertDesk: function(desk) {
+                var deskView = new DeskView({model: desk});
+                this.$('.desks').append(deskView.render().el);
+            },
+
+            assignUserToDesk: function(user) {
+
+                // set local user as main user of app
+                if (user.id == jsonVars.user._id) {
+                    app.set({'user': user});
+                }
+
+                // find the first empty desk
+                var desk = this.model.desks.find(function(desk) {
+                    return !desk.get('user');
+                });
+
+                // assign user to desk
+                desk.trigger('addUser', user);
+            },
+
+            removeUserFromDesk: function(user) {
+                // find the desk assigned to disconnected user
+                var desk = this.model.desks.find(function(desk) {
+                    return desk.get('user')._id == user._id
+                });
+
+                // unassign user
+                desk.trigger('removeUser', user);
+            },
+
+            render: function() {
+                $(this.el).html(this.template(this.model.toJSON()));
+
+                for (i = 0; i < this.model.get('classSizeLimit'); i++) {
+                    this.model.desks.add({});
+                }
+                
+                return this;
+            }
+        });
+
+        var Desk = Backbone.Model.extend({
+            defaults: {
+                user: null
+            }
+        });
+        
+        var DeskView = Backbone.View.extend({
+            initialize: function() {
+                _.bindAll(this, 'insertUser', 'removeUser');
+                this.model.bind('addUser', this.insertUser);
+                this.model.bind('removeUser', this.removeUser);
+            },
+            
+            template: _.template($('.desk-template').html()),
+
+            className: 'desk',
+            
+            insertUser: function(user) {
+                
+                this.model.set({'user': user})
+                var userView = new UserView({model: user});
+                $(this.el).append(userView.render().el).addClass(user.get('username'));
+            },
+
+            removeUser: function(user) {
+                this.model.unset('user');
+                $(this.el).removeClass(user.get('username'));
+            },
+
+            render: function() {
+                $(this.el).html(this.template(this.model.toJSON())).addClass('desk-'+ this.model.collection.indexOf(this.model));
+                return this;
+            }
+        });
+
+        var Desks = Backbone.Collection.extend({
+            model: Desk
+        });
+
+        var Message = Backbone.Model.extend({
+        
+        });
+
+        var MessageView = Backbone.View.extend({
+            initialize: function() {
+            
+            },
+
+            template: _.template($('.message-template').html()),
+
+            render: function() {
+                // For now, if there is no username,
+                // use the current user's username.
+                // TODO: figure out a more elegant model
+                // solution.
+            
+                if (!this.model.get('username')) {
+                    this.model.set({'username': app.get('user').get('username')});
+                }
+            
+                $(this.el).html(this.template(this.model.toJSON()));
+                return this;
+            }
+        });
+
+        var Messages = Backbone.Collection.extend({
+            initialize: function() {
+                _.bindAll(this, 'sendMessage');
+                this.bind('add', this.sendMessage);     
+            },
+
+            sendMessage: function(message) {
+                // Prevents the client from rebroadcasting a message
+                // received from the server.
+                if (message.get('username')) { return; }
+
+                console.log('chat should be sent here');
+                socket.emit('chat', message.toJSON());
+            }
+        });
+
+        var ChatLog = Backbone.Model.extend({
+            initialize: function() {
+                this.messages = new Messages;
+                this.messages.chatLog = this;
+            }
+        });
+
+        var ChatLogView = Backbone.View.extend({
+            initialize: function() {
+                _.bindAll(this, 'insertMessage'); 
+                this.model.messages.bind('add', this.insertMessage)
+            },
+
+            className: 'chat-log',
+
+            template: _.template($('.chat-log-template').html()),
+        
+            events: {
+                'click .tab': 'toggleChatDrawer'
+            },
+        
+            toggleChatDrawer: function(e) {
+                var $drawer = this.el;
+                var newVal = $drawer.css('top') === '0px' ? -226 : 0;
+                $drawer.css({'top':newVal});
+                $drawer.find('.tab-inner').text(newVal === 0 ? 'hide' : 'chat log');
+            },
+
+            insertMessage: function(message) {
+                console.log('messages collection', this.model.messages);
+                var messageView = new MessageView({model: message});
+                var username = message.get('username');
+                //TODO: make the UserViews listen to the chatlog
+                //and then handle this 
+                if (username) {
+                    var $chatBox = $('<div class="chat-box">'+message.get('content')+'</div>');
+                    $('.'+username+' .user').append($chatBox);
+                    setInterval(function() {
+                        $chatBox.fadeOut(2000, function(){
+                            $chatBox.remove();
+                        });
+                    }, 4000);
+                }
+ 
+                // Add message to the chatlog drawer
+                $(this.el).find('.pane').append(messageView.render().el).scrollTop($(messageView.el).offset().top + 99999);
+            
+            },
+        
+            render: function() {
+                $(this.el).html(this.template(this.model.toJSON()));
+                return this;
+            }
+        });
+
+
+        window.app = new App();
+        window.appView = new AppView({model: app, el: $('body')});
+        window.app.start({classSizeLimit: 20});
+
+        // socket communication
+        socket = io.connect('http://localhost:3001');
+        
+        socket.on('connected', function(data) {
+            app.users.add(data.users);
+            socket.emit('join', {username: jsonVars.user.username});
+        });
+        
+        socket.on('joined', function(user) {
+
+            app.users.add(user)
+            console.log(user.username+ ' joined!');
+        });
+        
+        socket.on('news', function (data) {
+            console.log(data);
+        });
+
+        socket.on('chat', function(data) {
+            console.log('chat '+ JSON.stringify(data));
+            app.chatLog.messages.add(data);
+        });
+
+        socket.on('disconnected', function(user) {
+            console.log(user.username + ' was disconnected');
+            app.users.remove(app.users.get(user._id));
+        });
+    }
 })();
--- server.js
+++ server.js
@@ -2,25 +2,28 @@ var express = require('express'),
     _ = require('underscore');
 
 // underscore template languate settings
-_.templateSettings = {
-    interpolate : /\{\{(.+?)\}\}/g, // {{ var }}
-    evaluate: /\{\%(.+?)\%\}/g // {% expression %}
-}; 
+// _.templateSettings = {
+//     interpolate : /\{\{(.+?)\}\}/g, // {{ var }}
+//     evaluate: /\{\%(.+?)\%\}/g // {% expression %}
+// }; 
 
 var conf = require('./conf');
 
-var everyauth = require('everyauth')
-  , Promise = everyauth.Promise
-  ,  mongooseAuth = require('mongoose-auth');
+var mongooseAuth = require('mongoose-auth');
 
-everyauth.debug = true;
+var mongoose = require('mongoose'),
+    conf = require('./conf'),
+    mongooseAuth = require('mongoose-auth'),
+    Schema = mongoose.Schema,
+    ObjectId = mongoose.SchemaTypes.ObjectId;
 
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
-  , ObjectId = mongoose.SchemaTypes.ObjectId;
 
-var UserSchema = new Schema({})
-  , User;
+var User;
+
+var UserSchema = new Schema({
+        username: String
+});
+
 
 
 UserSchema.plugin(mongooseAuth, {
@@ -33,7 +36,7 @@ UserSchema.plugin(mongooseAuth, {
     }
   , facebook: {
       everyauth: {
-          myHostname: 'http://local.host:3000'
+          myHostname: conf.hostname
         , appId: conf.fb.appId
         , appSecret: conf.fb.appSecret
         , redirectPath: '/'
@@ -41,7 +44,7 @@ UserSchema.plugin(mongooseAuth, {
     }
   , twitter: {
       everyauth: {
-          myHostname: 'http://local.host:3000'
+          myHostname: conf.hostname
         , consumerKey: conf.twit.consumerKey
         , consumerSecret: conf.twit.consumerSecret
         , redirectPath: '/'
@@ -69,7 +72,7 @@ UserSchema.plugin(mongooseAuth, {
     }
   , github: {
       everyauth: {
-          myHostname: 'http://local.host:3000'
+          myHostname: conf.hostname
         , appId: conf.github.appId
         , appSecret: conf.github.appSecret
         , redirectPath: '/'
@@ -77,12 +80,17 @@ UserSchema.plugin(mongooseAuth, {
     }
   , instagram: {
       everyauth: {
-          myHostname: 'http://local.host:3000'
+          myHostname: conf.hostname
         , appId: conf.instagram.clientId
         , appSecret: conf.instagram.clientSecret
         , redirectPath: '/'
       }
     }
+});
+
+UserSchema.pre('save', function(next) {
+    this.username = this._doc.twit.screenName || this._doc.fb.username;
+    next();
 });
 
 mongoose.model('User', UserSchema);
@@ -110,16 +118,17 @@ var DonationSchema = new Schema({
 
 mongoose.model('Donation', DonationSchema);
 
-mongoose.connect('mongodb://localhost/alumni');
+
+mongoose.connect('mongodb://localhost/classofkhan');
 
 User = mongoose.model('User');
 Project = mongoose.model('Project');
 
 var app = express.createServer(
-    express.bodyParser()
-  , express.cookieParser()
-  , express.session({ secret: '$3CR3#'})
-  , mongooseAuth.middleware()
+    express.bodyParser(),
+    express.cookieParser(),
+    express.session({ secret: '$3CR3#'}),
+    mongooseAuth.middleware()
 );
 
 app.use('/media', express.static(__dirname + '/media'));
@@ -139,10 +148,8 @@ app.register('.html', {
 
 app.get('/', function (req, res) {
     var templateVars = {};
-    Project.find({}, function(err, projects) {
-        templateVars.projects = projects;
-        res.render('index.html', templateVars);
-    });
+    var template = req.loggedIn ? 'index.html' : 'login.html';
+    res.render(template, templateVars);
 });
 
 app.get('/:projectSlug', function (req, res) {
@@ -163,30 +170,46 @@ app.get('/logout', function (req, res) {
     res.redirect('/');
 });
 
-// testing route...  should use front end url routing in the end
-app.get('/some-project', function (req, res) {
-  res.render('project.html');
-});
-
 mongooseAuth.helpExpress(app);
 
-app.listen(3000);
+app.listen(conf.port);
 
-var io = require('socket.io').listen(3001);
+var io = require('socket.io').listen(conf.port+1);
+
+var users = []
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+    socket.emit('connected', {users: users});
+    
+    socket.on('join', function(data) {
+        User.findOne({username: data.username}, function(err, user) {
+            socket.set('user', user);
+            users.push(user);
+            io.sockets.emit('joined', user);
+        });
+    });
+    
+    socket.emit('news', { hello: 'world' });
   
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-  
-  socket.on('project:create', function (data) {
-    console.log(data);
-    var project = new Project(data);
-    project.save();
-  });
+    socket.on('chat', function (data) {
+        console.log(data);
+        socket.get('user', function(err, user) {
+            socket.broadcast.emit('chat', {content: data.content, 'username': user.username});
+        });
+    });
+
+    socket.on('disconnect', function(data) {
+        socket.get('user', function(err, user) {
+            var index = users.indexOf(user);
+            if (index > -1) { users.splice(index, 1) }
+            
+            if (user) {
+                socket.broadcast.emit('disconnected', user);
+            }
+        });
+    });
 });
+
+
+
+
